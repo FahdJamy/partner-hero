3f69b83d997653113fcbedba91088f3cfbcf94a1
fahdjamy <fahdjamy2@gmail.com>
2020-07-27 11:14:26 +0300

    Updates

---
+++ precache-manifest.dfc6264bd755af2991524df9cf79eca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3054e682027a8772e5d0984b62d7b952",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e917ad5fdcc8e25232",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "09a6c12f7d14b21338ca",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "01e917ad5fdcc8e25232",
+    "url": "/static/js/2.bd05b463.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.bd05b463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a6c12f7d14b21338ca",
+    "url": "/static/js/main.774bbe65.chunk.js"
+  },
+  {
+    "revision": "963f4fb3bc5033014871",
+    "url": "/static/js/runtime-main.8b4e5cdb.js"
+  }
+]);
